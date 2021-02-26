855f1959b0ff42336ab847e43a81bc858533aae6
Sameer Al Harbi <smrhrbi@gmail.com>
2021-02-27 02:14:53 +0300

    mac

--- src/util/abap-system.js
+++ src/util/abap-system.js
@@ -1,12 +1,13 @@
 // ABAP system RFC connection parameters
 const abapSystem = {
-    // dest: process.env.SAP_DESTINATION_NAME,
+    connectionParameters: { 
     user: process.env.SAP_USER,
     passwd: process.env.SAP_PASSWORD,
     ashost: process.env.SAP_SERVER_HOST,
     sysnr: process.env.SAP_SYSTEM_NUMBER,
     client: process.env.SAP_CLIENT,
-    lang: process.env.SAP_LANGUAGE
+    lang: process.env.SAP_LANGUAGE, 
+    dest: process.env.SAP_DESTINATION_NAME } 
 };
 
 module.exports = abapSystem;
