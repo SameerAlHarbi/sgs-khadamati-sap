bf1cffe332f042cf8966e42f0bf5d50f56f6f17b
Sameer Al Harbi <smrhrbi@gmail.com>
2021-02-27 14:44:37 +0300

    win

--- src/routers/countries.router.js
+++ src/routers/countries.router.js
@@ -3,16 +3,16 @@ const countriesController = require('../controllers/countries.controller');
 
 const Router = express.Router();
 
-// /countries?lang=A => GET
+// /countries?{lang=A} => GET
 Router.get('/', countriesController.getAllCountries);
 
-// /countries/{countryCode}?lang=A => GET
+// /countries/{countryCode}?{lang=A} => GET
 Router.get('/:countryCode', countriesController.getCountryByCode);
 
-// /countries/{countryCode}/cities?lang=A => GET
+// /countries/{countryCode}/cities?{lang=A} => GET
 Router.get('/:countryCode/cities', countriesController.getAllCities);
 
-// /countries/{countryCode}/cities/{cityCode}?lang=A => GET
+// /countries/{countryCode}/cities/{cityCode}?{lang=A} => GET
 Router.get('/:countryCode/cities/:cityCode', countriesController.getCityByCode);
 
 module.exports = Router;
