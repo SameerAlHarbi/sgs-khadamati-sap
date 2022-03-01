f0e76de4782f3f1e33576bfbde0fd9b68a328109
OsamaNajjar <42926788+OsamaNajjar@users.noreply.github.com>
2022-03-01 09:35:21 +0300

    fix commintes

--- src/routers/delegations.router.js
+++ src/routers/delegations.router.js
@@ -3,7 +3,7 @@ const delegationsController = require('../controllers/delegations.controller');
 
 const Router = express.Router();
 
-// /delegations/delegationsTypes?{lang=A} => GET
+// /delegations/Types?{lang=A} => GET
 Router.get('/types', delegationsController.getDelegationTypes);
 
 module.exports = Router;
